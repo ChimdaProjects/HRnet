1f5c59136d05d694139ca8bcf428611570fe1d32
Chimda B <chimdabrignoli@gmail.com>
2023-06-07 23:25:26 +0200

    change style and favicon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-logo">
         <Link to="/">
-          <img src={logo} alt="logo of hrnet" className="navbar-logo-img" />
+          <img src={logo} width="140px" height="140px" alt="logo of hrnet" className="navbar-logo-img" />
         </Link>
       </div>
       <ul className="navbar-content">
